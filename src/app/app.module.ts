470fb60a9655b5f8bc5e18e0658f2e2dfdc79836
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-03-21 21:53:48 -0400

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
