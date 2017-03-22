570ad66c87d3390b9e97c7409f1f5286e1eebb2d
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-03-22 09:10:50 -0400

    finished landing pag & cities page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { CitiesPageComponent } from './cities-page/cities-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
-  // {
-  //   //When you visit '/' it will be using the
-  //   //phone list component
-  //   path: '/',
-  //   component: LandingPageComponent
-  // },
+  {
+    //When you visit '/' it will be using the
+    //phone list component
+    path: '',
+    component: LandingPageComponent
+  },
   {
     path: 'cities',
     component: CitiesPageComponent
   }
-
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-<<<<<<< HEAD
+
 import { CitiesPageComponent } from './cities-page/cities-page.component';
-=======
 import { LandingPageComponent } from './landing-page/landing-page.component';
->>>>>>> 3ca3a43a61e176ea09b064405f9007c868fa4878
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-<<<<<<< HEAD
-    CitiesPageComponent
-=======
+    CitiesPageComponent,
     LandingPageComponent
->>>>>>> 3ca3a43a61e176ea09b064405f9007c868fa4878
   ],
   imports: [
     BrowserModule,
