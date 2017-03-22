40c1db79f286e4e1ead4d7c4f4757b547c4279fc
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-03-21 20:41:01 -0400

    Angular project on foot. Working nav bar complete, working on making it smart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
