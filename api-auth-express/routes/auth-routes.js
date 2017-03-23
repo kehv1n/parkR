22b55349990a1f874ab0a45b197d345896072866
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-03-23 17:22:49 -0400

    lol

--- api-auth-express/routes/auth-routes.js
+++ api-auth-express/routes/auth-routes.js
@@ -11,18 +11,21 @@ authRoutes.post('/signup', (req, res, next) => {
   const nickname = req.body.nickname;
   const password = req.body.password;
 
+  //if blank
   if(!username || password) {
     res.status(400).json({message: 'Provide the username and password, dummy'});
     return;
   }
 
-  User.findOne({username }, '_id', (err, foundUser) => {
 
+  User.findOne({username }, '_id', (err, foundUser) => {
+    //if error in the query
     if (err) {
       res.status(500).json({message: 'Something went wrong, sorry'});
       return;
     }
 
+    //if user found
     if (foundUser) {
       res.status(400).json({message: 'The username already exists'});
       return;
@@ -37,13 +40,16 @@ authRoutes.post('/signup', (req, res, next) => {
       encryptedPassword: hashPass,
       nickname
     });
-
+    //save user
     theUser.save((err) => {
+      //if the save errors..
       if (err) {
         res.status(400).json({message: 'Something went wrong...'});
         return;
       }
-      req.login(theUser, (err) => {  //if all else passes, automatically log in the user..
+      req.login(theUser, (err) => {
+         //if all else passes, automatically log in the user..
+         // if login fucks up
         if (err){
           res.status(400).json({message: 'Something went wrong...'});
           return;
@@ -52,6 +58,57 @@ authRoutes.post('/signup', (req, res, next) => {
       });
     });
   });
+});
+
+authRoutes.post('/login', (req, res, next) => {
+  const passportFunction = passport.authenticate('local',
+  (err, theUser, failureDetails) => {
+    if (err) {
+      res.status(500).json({message: 'Something went wrong'});
+      return;
+
+    }
+
+    if (!theUSer) {
+      res.status(401).json({message: failureDetails});
+      return;
+    }
+
+    req.login(theUser, (err) => {
+      if (err) {
+        res.status(500).json({message: 'Something went wrong'});
+        return;
+      }
+      res.status(200).json(req.user);
+    });
+  });
+});
+
+authRoutes.post('/logout', (req, res, next) => {
+  req.logout();
+
+  res.status(200).json({message: 'Success'});
+});
+
+authRoutes.get('/loggedin', (req, res, next) => {
+  //if the user is logged in
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  //if the user is not logged in
+  res.status(401).json({message: 'Unauthorized.'});
+});
+
+function gtfoIfNotLogged(req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.status(403).json({message: 'FORBIDDEN'});
+    return;
+
+  }
+}
+authRoutes.get('/private',gtfoIfNotLogged, (req, res, next) => {
+  res.json({ message: 'Todays lucky number 76787'});
 });
 
 module.exports = authRoutes;
